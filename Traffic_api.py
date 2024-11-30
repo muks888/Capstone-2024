2e6e62408fc354f378c1c40f5f0343bbe5e7a211
muks888 <67940699+muks888@users.noreply.github.com>
2024-12-01 00:12:20 +0530

    Create Traffic_api.py

---
+++ Traffic_api.py
@@ -0,0 +1,28 @@
+import requests
+import smtplib 
+
+# API key
+api_key ='<key>'
+
+# home address input
+#home = input("Enter a home address\n") 
+home="domlur" 
+# work address input
+#work = input("Enter a work address\n") 
+work="sarjapur" 
+# base url
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
+
+# get response
+r = requests.get(url + "origins=" + home + "&destinations=" + work + "&key=" + api_key) 
+ 
+# return time as text and as seconds
+time = r.json()["rows"][0]["elements"][0]["duration"]["text"]       
+seconds = r.json()["rows"][0]["elements"][0]["duration"]["value"]
+distance = r.json()["rows"][0]["elements"][0]["distance"]["text"]
+
+# print the travel time
+print("\nThe total travel time from source  to dest is ", time)
+print("The total travel time from source to dest is ", distance)
+print("\n 17 miles per hours is fast. That is there is no traffic ")
+print("NO")
